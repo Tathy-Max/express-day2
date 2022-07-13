7872262d60452062062dc41d249685eaedbd0583
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-07-12 20:21:01 -0400

    routes done

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+//importing express
 const express = require("express");
+//initializing express
 const app = express();
-
+//calling express.json method to read and return json files
 app.use(express.json());
+
+//creating routes
+const clientsRouter = require("./routes/clients.routes");
+app.use("/clients", clientsRouter);
+
+const adressessRouter = require("./routes/adresses.routes");
+app.use("/adresses", adressessRouter);
+
+//creating CRUD
 
 app.post("/create", () => {});
 
+//server port listening
 app.listen(4000, () => {
   console.log("servidor rodando na porta 4000");
 });
